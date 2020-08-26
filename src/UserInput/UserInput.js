7a7e2cede1a7a0cbcb4a8fb7d0a14ca335d943d1
dev-carlos garcia <carlosrenegarcia.a4b@gmail.com>
2020-08-26 16:06:09 +0200

    [IMP] fix file content

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './UserInput.css';
+const userInput=( props )=>{
+    return  (
+        <div className="UserInput">
+
+        </div>
+    )
+}
+export default  userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './UserInput.css';
+const serOutput=( props )=>{
+    return  (
+        <div className="UserOutput">
+        </div>
+    )
+}
+export default  UserOutput;
